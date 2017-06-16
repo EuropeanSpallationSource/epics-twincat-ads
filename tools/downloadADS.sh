7f050c4da1a38bbfd2a67f7c28b1d3cb9c65d28d
Torsten Bögershausen <torsten.bogershausen@esss.se>
2017-06-16 08:27:45 +0200

    ADS: Use version shown "Info:  Connected to"
    Change the Sockets.cpp to LOG "Connected to".
    Download & use it.

--- tools/downloadADS.sh
+++ tools/downloadADS.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-ADSCOMMIT=c64a050d880034435023bb095495c6f05c0fa18a
+ADSCOMMIT=35058e7ed6c2666af5ee6f390a170f32b11143
 echo "$0 PWD=$PWD"
 
 if test -d ADS; then
 	echo "Note: ADS is already cloned"
 else
 	(
-		git clone https://github.com/Beckhoff/ADS.git ADS.tmp &&
+		git clone https://github.com/EuropeanSpallationSource/ADS.git ADS.tmp &&
  		cd ADS.tmp &&
 		git checkout $ADSCOMMIT &&
 		cd .. &&
