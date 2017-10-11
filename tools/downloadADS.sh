75ef0f293c29bd90c5559c33a58338ff04dcfbb8
Torsten Bögershausen <torsten.bogershausen@esss.se>
2017-10-11 16:26:06 +0200

    ADS as submodule
    As we already clone the ADS as a Git, (and ignore it), it is better
    to do this "officially" by using a Git submodule.
    
    Keep the rest "as is", if the user doesn't use
    "git clone --recursive" then "make" will help us out.

--- tools/downloadADS.sh
+++ tools/downloadADS.sh
@@ -3,13 +3,14 @@ ADSCOMMIT=35058e7ed6c2666af5ee6f390a170f32b11143
 echo "$0 PWD=$PWD"
 
 if test -d ADS; then
-	echo "Note: ADS is already cloned"
+  echo "Note: ADS is already cloned"
 else
-	(
-		git clone https://github.com/EuropeanSpallationSource/ADS.git ADS.tmp &&
- 		cd ADS.tmp &&
-		git checkout $ADSCOMMIT &&
-		cd .. &&
-		mv ADS.tmp/ ADS/
-	)
+  (
+    git submodule update
+    (
+        cd ADS &&
+        (git remote -v | grep Beckhoff) ||
+        git remote add Beckhoff https://github.com/Beckhoff/ADS.git
+    )
+  )
 fi
