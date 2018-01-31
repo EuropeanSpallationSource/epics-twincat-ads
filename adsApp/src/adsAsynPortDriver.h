47d9d93bdce442f17c0555d4fce1c81794fbc5a8
vagrant <vagrant@localhost.localdomain>
2018-01-31 23:00:49 +0100

    Cleanup in formatting.

--- adsApp/src/adsAsynPortDriver.h
+++ adsApp/src/adsAsynPortDriver.h
@@ -11,7 +11,6 @@
 #define MAX_FIELD_CHAR_LENGTH 128
 #define ADR_COMMAND_PREFIX ".ADR."
 
-
 typedef struct adsParamInfo{
   char *recordName;
   char *recordType;
@@ -36,43 +35,72 @@ typedef struct adsParamInfo{
 
 class adsAsynPortDriver : public asynPortDriver {
 public:
-  adsAsynPortDriver(const char *portName,
-                      const char *ipaddr,
-                      const char *amsaddr,
-                      unsigned int amsport,
-                      int paramTableSize,
-                      unsigned int priority,
-                      int autoConnect,
-                      int noProcessEos);
+  adsAsynPortDriver(const char *portName,const char *ipaddr,
+                                    const char *amsaddr,
+                                    unsigned int amsport,
+                                    int paramTableSize,
+                                    unsigned int priority,
+                                    int autoConnect,
+                                    int noProcessEos);
   virtual ~adsAsynPortDriver();
   virtual void report(FILE *fp, int details);
   virtual asynStatus disconnect(asynUser *pasynUser);
   virtual asynStatus connect(asynUser *pasynUser);
-  virtual asynStatus drvUserCreate(asynUser *pasynUser,const char *drvInfo,const char **pptypeName,size_t *psize);
-  virtual asynStatus writeOctet(asynUser *pasynUser, const char *value, size_t maxChars, size_t *nActual);
-  virtual asynStatus readOctet(asynUser *pasynUser, char *value, size_t maxChars,size_t *nActual, int *eomReason);
-  virtual asynStatus writeInt32(asynUser *pasynUser, epicsInt32 value);
-  virtual asynStatus writeFloat64(asynUser *pasynUser, epicsFloat64 value);
-  virtual asynStatus readFloat64(asynUser *pasynUser, epicsFloat64 *value);
-  virtual asynStatus readInt8Array(asynUser *pasynUser, epicsInt8 *value,size_t nElements, size_t *nIn);
-  virtual asynStatus readInt16Array(asynUser *pasynUser, epicsInt16 *value,size_t nElements, size_t *nIn);
-  virtual asynStatus readInt32Array(asynUser *pasynUser, epicsInt32 *value,size_t nElements, size_t *nIn);
-  virtual asynStatus readFloat32Array(asynUser *pasynUser, epicsFloat32 *value,size_t nElements, size_t *nIn);
-  virtual asynStatus readFloat64Array(asynUser *pasynUser, epicsFloat64 *value,size_t nElements, size_t *nIn);
-  asynStatus setCfgData(const char *portName,
-                          const char *ipaddr,
-                          const char *amsaddr,
-                          unsigned int amsport,
-                          unsigned int priority,
-                          int noAutoConnect,
-                          int noProcessEos);
+  virtual asynStatus drvUserCreate(asynUser *pasynUser,
+                                    const char *drvInfo,
+                                    const char **pptypeName,
+                                    size_t *psize);
+  virtual asynStatus writeOctet(asynUser *pasynUser,
+                                    const char *value,
+                                    size_t maxChars,
+                                    size_t *nActual);
+  virtual asynStatus readOctet(asynUser *pasynUser,
+                                    char *value,
+                                    size_t maxChars,
+                                    size_t *nActual,
+                                    int *eomReason);
+  virtual asynStatus writeInt32(asynUser *pasynUser,
+                                    epicsInt32 value);
+  virtual asynStatus writeFloat64(asynUser *pasynUser,
+                                    epicsFloat64 value);
+  virtual asynStatus readFloat64(asynUser *pasynUser,
+                                    epicsFloat64 *value);
+  virtual asynStatus readInt8Array(asynUser *pasynUser,
+                                    epicsInt8 *value,
+                                    size_t nElements,
+                                    size_t *nIn);
+  virtual asynStatus readInt16Array(asynUser *pasynUser,
+                                    epicsInt16 *value,
+                                    size_t nElements,
+                                    size_t *nIn);
+  virtual asynStatus readInt32Array(asynUser *pasynUser,
+                                    epicsInt32 *value,
+                                    size_t nElements,
+                                    size_t *nIn);
+  virtual asynStatus readFloat32Array(asynUser *pasynUser,
+                                     epicsFloat32 *value,
+                                     size_t nElements,
+                                     size_t *nIn);
+  virtual asynStatus readFloat64Array(asynUser *pasynUser,
+                                     epicsFloat64 *value,
+                                     size_t nElements,
+                                     size_t *nIn);
+  asynStatus setCfgData(const char *portName,const char *ipaddr,
+                                     const char *amsaddr,
+                                     unsigned int amsport,
+                                     unsigned int priority,
+                                     int noAutoConnect,
+                                     int noProcessEos);
   asynUser *getTraceAsynUser();
 protected:
 
 private:
-  asynStatus getRecordInfoFromDrvInfo(const char *drvInfo,adsParamInfo *paramInfo);
-  //asynStatus getParamDataType(const char *drvInfo,asynParamType *type);
-  asynStatus parsePlcInfofromDrvInfo(const char* drvInfo,adsParamInfo *paramInfo);
+  asynStatus getRecordInfoFromDrvInfo(const char *drvInfo,
+                                     adsParamInfo *paramInfo);
+  //asynStatus getParamDataType(const char *drvInfo,
+  //                                 asynParamType *type);
+  asynStatus parsePlcInfofromDrvInfo(const char* drvInfo,
+                                     adsParamInfo *paramInfo);
   asynParamType dtypStringToAsynType(char *dtype);
   int getAmsPortFromDrvInfo(const char* drvInfo);
   void printParamInfo(adsParamInfo *paramInfo);
