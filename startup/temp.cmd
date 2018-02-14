c9628552a4e6fe20e70bfac3fed2158475695a68
Anders Sandström <anders.sandstrom@esss.se>
2018-02-14 14:55:55 +0100

    Cleanup in startup files.

--- startup/temp.cmd
+++ startup/temp.cmd
@@ -11,22 +11,19 @@ require ads,anderssandstrom
 # 3. Start with: iocsh st.adsTestAxisRecord.cmd
 # 
 ##############################################################################
-############# Configure device (<ASYN PORT>, <IP_of_PLC>,<AMS_of_PLC>,<Default_ADS_Port>,<Not_used>,<Not_used>,<Not_used>):
 ## Configure devices
-#drvAsynAdsPortConfigure("ADS_1","192.168.88.60","5.40.216.206.1.1",851,0, 0,0)
-# MCU10adsAsynPortDriverConfigure("ADS_1","192.168.88.60","5.40.216.206.1.1",851,1000, 0, 0,0)
-# 1. Asyn port
-# 2. IP
-# 3. AMS of plc
-# 4. Default ams port (851 for plc 1, 852 plc 2 ...)
-# 5. Parameter table size (max parameters)
-# 6. priority
-# 7. disable auto connnect
-# 8. noProcessEOS
-# 9. default sample time ms
-# 10. max delay time ms (buffer time in plc)
-# 11. ADS command timeout in ms
-# 12. default time source (EPICS=0,PLC=1) NOTE record TSE field need to be set to -2 for timestamp in asyn ("field(TSE, -2)")
+# 1. Asyn port name                         : "ADS_1"
+# 2. IP                                     : "192.168.88.10"
+# 3. AMS of plc                             : "192.168.88.11.1.2"
+# 4. Default ams port                       : 851 for plc 1, 852 plc 2 ...
+# 5. Parameter table size (max parameters)  : 1000 example
+# 6. priority                               : 0
+# 7. disable auto connnect                  : 0 (autoconnect enabled)
+# 8. noProcessEOS                           : 0
+# 9. default sample time ms                 : 500
+# 10. max delay time ms (buffer time in plc): 1000
+# 11. ADS command timeout in ms             : 1000  
+# 12. default time source (EPICS=0,PLC=1).  : 1 (PLC) NOTE: record TSE field need to be set to -2 for timestamp in asyn ("field(TSE, -2)")
 
 adsAsynPortDriverConfigure("ADS_1","192.168.88.44","192.168.88.44.1.1",851,1000, 0, 0,0,50,100,1000,1)
 
@@ -34,8 +31,6 @@ asynOctetSetOutputEos("ADS_1", -1, "\n")
 asynOctetSetInputEos("ADS_1", -1, "\n")
 
 asynSetTraceMask("ADS_1", -1, 0x01)
-asynSetTraceIOMask("ADS_1", -1, 0x44)
-asynSetTraceInfoMask("ADS_1", -1, 15)
 
 #############################################################################
 
