243ca3036740e529c779af73ef542dfb5aa63619
Torsten Bögershausen <torsten.bogershausen@esss.se>
2018-03-01 15:15:52 +0100

    downloadADS.sh: Use the appropriate git submodule commands
    When the use did not clone --recursice, the ADS/ is empty.
    Use "git submodule" to update it,

--- tools/downloadADS.sh
+++ tools/downloadADS.sh
@@ -3,14 +3,6 @@ echo "$0 PWD=$PWD"
 
 if ! test -e ADS/AdsLib/AdsLib.h; then
 (
-	git rm --cached ADS || :
-	git submodule deinit ADS || :
-	mv ADS ADS.$$ || :
-	git submodule add -f https://github.com/Beckhoff/ADS.git ADS || :
-	cd ADS &&
-	(
-			git reset --hard || :
-	)
-	git add ADS
+  git submodule init ADS && git submodule update ADS		
 )
 fi
