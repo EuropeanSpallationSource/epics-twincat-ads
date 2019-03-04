aebb422e908e8a3935dc62c63960e0c4cfd57770
Anders Sandström <anders.sandstrom@esss.se>
2019-03-04 14:37:15 +0100

    Simple GUI for testrig..

--- startup/startTargetTest.cmd
+++ startup/startTargetTest.cmd
@@ -11,21 +11,18 @@ require ads,dev
 # 7.  disable auto connnect                  :  0 (autoconnect enabled)
 # 8.  default sample time ms                 :  500
 # 9.  max delay time ms (buffer time in plc) :  1000
-# 10. ADS command timeout in ms             :  1000  
-# 11. default time source (PLC=0,EPICS=1)   :  0 (PLC) NOTE: record TSE field need to be set to -2 for timestamp in asyn ("field(TSE, -2)")
+# 10. ADS command timeout in ms              :  1000  
+# 11. default time source (PLC=0,EPICS=1)    :  0 (PLC) NOTE: record TSE field need to be set to -2 for timestamp in asyn ("field(TSE, -2)")
 
-adsAsynPortDriverConfigure("TARGET_ADS","192.168.88.200","5.46.8.38.1.1",852,1000,0,0,50,100,1000,0)
+adsAsynPortDriverConfigure("TARGET_ADS","192.168.88.200","5.46.8.38.1.1",852,1000,0,0,50,100,5000,0)
 
 asynOctetSetOutputEos("TARGET_ADS", -1, "\n")
 asynOctetSetInputEos("TARGET_ADS", -1, "\n")
 asynSetTraceMask("TARGET_ADS", -1, 0x41)
 
 ##############################################################################
-############# Load records
-dbLoadRecords("target.db","P=TARGET_DU:,PORT=TARGET_ADS")
-dbLoadRecords("indraDriveStatus.db","P=TARGET_DU:,PORT=TARGET_ADS")
-dbLoadRecords("indraDriveCommand.db","P=TARGET_DU:,PORT=TARGET_ADS")
-dbLoadRecords("brake.db","P=TARGET_DU:,PORT=TARGET_ADS")
+############# Load records (asyn direct I/O intr):
+dbLoadRecords("target.db","P=ADS_IOC:ASYN:,PORT=TARGET_ADS")
 
 ##############################################################################
 ############# Usefull commands
