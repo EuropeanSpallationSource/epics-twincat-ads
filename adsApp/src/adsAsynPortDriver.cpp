1907136c88f462f1a376e2748810a614fba58dcb
Michael Browne <mcbrowne@slac.stanford.edu>
2021-06-18 15:14:13 -0700

    Don't play with handles if we're an absolute address.

--- adsApp/src/adsAsynPortDriver.cpp
+++ adsApp/src/adsAsynPortDriver.cpp
@@ -928,10 +928,12 @@ asynStatus adsAsynPortDriver::updateParamInfoWithPLCInfo(adsParamInfo *paramInfo
     }
   }
 
-  adsReleaseSymbolicHandle(paramInfo,true); //try to delete
-  status=adsGetSymHandleByName(paramInfo);
-  if(status!=asynSuccess){
-    return asynError;
+  if (!paramInfo->isAdrCommand) {
+      adsReleaseSymbolicHandle(paramInfo,true); //try to delete
+      status=adsGetSymHandleByName(paramInfo);
+      if(status!=asynSuccess){
+	  return asynError;
+      }
   }
 
   if(paramInfo->isIOIntr){
