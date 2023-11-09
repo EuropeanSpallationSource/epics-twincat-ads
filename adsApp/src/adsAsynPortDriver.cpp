791ce54d176344310065030680c5b530c3b56a56
Torsten Bögershausen <torsten.bogershausen@esss.se>
2023-11-09 17:25:24 +0100

    adsAsynPortDriver.cpp: Untabify

--- adsApp/src/adsAsynPortDriver.cpp
+++ adsApp/src/adsAsynPortDriver.cpp
@@ -932,7 +932,7 @@ asynStatus adsAsynPortDriver::updateParamInfoWithPLCInfo(adsParamInfo *paramInfo
       adsReleaseSymbolicHandle(paramInfo,true); //try to delete
       status=adsGetSymHandleByName(paramInfo);
       if(status!=asynSuccess){
-	  return asynError;
+          return asynError;
       }
   }
 
